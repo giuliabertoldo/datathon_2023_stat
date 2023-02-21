7727cec756f9f3ae0a4990be42be81b125f25803
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-02-21 07:58:23 +0100

    Obtain list of all artists

--- main_centrality_algo.py
+++ main_centrality_algo.py
@@ -6,66 +6,26 @@ driver = GraphDatabase.driver(
   "bolt://54.161.134.53:7687",
   auth=basic_auth("neo4j", "loaf-acronyms-rifling"))
 
-cypher_query = '''
-call db.schema.visualization
-'''
+# Return all artists
+def get_artists(tx):
+    result = tx.run("MATCH (p:Artist) RETURN p.name AS name")
+    records = list(result)  # a list of Record objects
+    summary = result.consume()
+    return records, summary 
 
 with driver.session(database="neo4j") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      limit="10").data())
-  for record in results:
-    print(record['count'])
+    records, summary = session.execute_read(get_artists)
+
+    # Summary information
+    print("The query `{query}` returned {records_count} records in {time} ms.".format(
+        query=summary.query, records_count=len(records),
+        time=summary.result_available_after
+    ))
+
+    # Loop through results and do something with them
+    for person in records:
+        print(person.data())  # obtain record as dict
+
 
 driver.close()
 
-# ARTISTS 
-
-# How many Artists? 616
-cypher_query = '''
-MATCH (n:Artist)
-RETURN count(*)
-'''
-
-# How many relationships between artistics? 63 
-cypher_query = '''
-MATCH (n:Artist)-[r]->(m:Artist)
-RETURN count(r)
-'''
-
-# Algorithm: Harmonic centrality - Artists 
-cypher_query = '''
-CALL gds.graph.project('myGraph', 'Artist', 'APPRENTICE_OF')
-'''
-cypher_query = '''
-CALL gds.alpha.closeness.harmonic.stream('myGraph', {})
-YIELD nodeId, centrality
-RETURN gds.util.asNode(nodeId).name AS user, centrality
-ORDER BY centrality DESC
-'''
-
-
-
-# ARTWORKS
-## How many recommends unidirectional? 5774
-cypher_query = '''
-MATCH (n:Artwork)-[r:RECOMMENDS]->(m)
-RETURN count(r)
-'''
-
-## How many artworks? 10517 
-cypher_query = '''
-MATCH (n:Artwork)
-RETURN count(n)
-'''
-
-## Algorithm: Harmonic centrality - Artists 
-cypher_query = '''
-CALL gds.graph.project('myGraphWork', 'Artwork', 'RECOMMENDS')
-'''
-cypher_query = '''
-CALL gds.alpha.closeness.harmonic.stream('myGraphWork', {})
-YIELD nodeId, centrality
-RETURN gds.util.asNode(nodeId).name AS user, centrality
-ORDER BY centrality DESC
-'''
