ecde6d876ebec5fd9f0be5c99825792cd641a9f2
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-02-21 12:07:32 +0100

    Second script to run queries from Python to neo4j

--- main_centrality_algo.py
+++ main_centrality_algo.py
@@ -26,6 +26,30 @@ with driver.session(database="neo4j") as session:
     for person in records:
         print(person.data())  # obtain record as dict
 
+# Run Harmonic Centrality algorithm 
+def harmonic(tx):
+    result = tx.run("
+    CALL gds.alpha.closeness.harmonic.stream('myGraph', {})
+ YIELD nodeId, centrality
+ RETURN gds.util.asNode(nodeId).name AS user, centrality
+ ORDER BY centrality DESC")
+    records = list(result)  # a list of Record objects
+    summary = result.consume()
+    return records, summary 
+
+with driver.session(database="neo4j") as session:
+    records, summary = session.execute_read(get_artists)
+
+    # Summary information
+    print("The query `{query}` returned {records_count} records in {time} ms.".format(
+        query=summary.query, records_count=len(records),
+        time=summary.result_available_after
+    ))
+
+    # Loop through results and do something with them
+    for person in records:
+        print(person.data())  # obtain record as dict
+
 
 driver.close()
 
