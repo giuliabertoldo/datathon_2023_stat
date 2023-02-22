7dc0ddc838718885544cf52c709eb8a89562d634
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-02-22 11:15:21 +0100

    Update

--- data_cleaning.py
+++ data_cleaning.py
@@ -22,15 +22,15 @@ artists['name'] = artists['name'].str.strip()
 
 # create last_name variables 
 for i in range(1, 6, 1):
-    artists.loc[artists['name'].str.split().str.len() == i, 'last name'] = artists['name'].str.split().str[-1]
+    artists.loc[artists['name'].str.split().str.len() == i, 'last_name'] = artists['name'].str.split().str[-1]
 # check that no null values are in last_name
-sum(artists['last name'].isnull())
+sum(artists['last_name'].isnull())
 
 # which rows have same last_name?
-duplicated = artists[artists['last name'].duplicated()]
+duplicated = artists[artists['last_name'].duplicated()]
 
-# keep only one artist with specified last name
-artists = artists.drop_duplicates(subset='last name')
+# keep only one artist with specified last_name
+artists = artists.drop_duplicates(subset='last_name')
 
 
  # Save in csv file 
