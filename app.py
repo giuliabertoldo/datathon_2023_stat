2c0e144b43397131fefff0e4a77d3250b28d08b7
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-02-22 21:06:25 +0100

    Correct index for second part of code

--- app.py
+++ app.py
@@ -2,26 +2,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
 
+
+#read csv files
 artists = pd.read_csv(r'artists.csv')
 apprenticeship = pd.read_csv(r'appr.csv')
 
 
-
-
-#artists = artists.sort_values(by='name')
-#artists = artists[34:-1]
-#artists = artists.sort_values(by='id')
-#artists = artists.sort_values(by='name')
-
 df = pd.DataFrame(artists)
 name1=df['name']
-#name1=name1.sort_values()
-#name1=name1[34:]
 
 
-st.title('Find the students and the teachers')
+#give a title to the app
+st.title('Relation between Artists')
 
 
 option1 = st.selectbox(
@@ -31,72 +24,29 @@ option1 = st.selectbox(
 str(option1)
 
 
-#option1 = "option1"
-
-
-#if(st.button('Find')):
-#    st.write("You selected", option1, "and", option2)
-
-#if(st.button('Find')):
-#    try:
-#        l=[]
-#        findId = artists[artists['name']==option1]['id'].index.tolist()
-
-    #print(findId)
-#        i=0
-#        while i < len(apprenticeship):
-#            if apprenticeship['teacher_id'][i]==findId[0]:
-#            #print([apprenticeship['teacher_id'][i]]) 
-#                l = l + [apprenticeship['student_id'][i]]
-#            else:
-#                pass
-#            i = i + 1
-#    
-#       l = [int(x) for x in l]
-#        final=[]
-#        for j in l:
-#            art_new=artists.to_numpy()
-#            final = final + [art_new[j][1]]
-#        s = ''
-#        if final == []:
-#            st.error('No students')
-#        else:
-#            for i in final:
-#                s += "- " + i + "\n"
-#            st.write('Students:')
-#            s
-#    except:
-#        pass
-    
-
 
 
 
 if(st.button('Find')):
     try:
+
+        #find students
         l=[]
         findId = [artists[artists['name']==option1]['id'].iloc[0]]
 
-        #df_temp = pd.DataFrame(artists[artists['name']==option1]['id'])
-        #findId = [df_temp['id'].iloc[0]]
-
-
-    #print(findId)
         i=0
         while i < len(apprenticeship):
             if apprenticeship['teacher_id'][i]==findId[0]:
-                # print([apprenticeship['teacher_id'][i]]) 
                 l = l + [apprenticeship['student_id'][i]]
             else:
                 pass
             i = i + 1
-            # print(l)
     
         l = [int(x) for x in l]
         final=[]
         for j in l:
             art_new=artists.to_numpy()
-            final = final + [art_new[j][2]]
+            final = final + [artists[artists['id']==j]['name'].iloc[0]]
         s = ''
         if final == []:
             st.error('No students')
@@ -106,15 +56,13 @@ if(st.button('Find')):
             st.write('Students:')
             s
 
-        #professors
+        #find professors
         l2=[]
-        findId2 = artists[artists['name']==option1]['id'].index.tolist()
+        findId2 = [artists[artists['name']==option1]['id'].iloc[0]]
 
-    #print(findId)
         i=0
         while i < len(apprenticeship):
             if apprenticeship['student_id'][i]==findId2[0]:
-            #print([apprenticeship['teacher_id'][i]]) 
                 l2 = l2 + [apprenticeship['teacher_id'][i]]
             else:
                 pass
@@ -123,8 +71,7 @@ if(st.button('Find')):
         l2 = [int(x) for x in l2]
         final2=[]
         for j in l2:
-            art_new2=artists.to_numpy()
-            final2 = final2 + [art_new2[j][2]]
+            final2 = final2 + [artists[artists['id']==j]['name'].iloc[0]]
         s2 = ''
         if final2 == []:
             st.error('No teachers')
