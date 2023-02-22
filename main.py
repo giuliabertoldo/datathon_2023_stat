1b9631996e41d513314cc5b102ec0bac8bd1672d
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-02-22 13:38:42 +0100

    Add visualization

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # SOURCE: https://towardsdatascience.com/neo4j-cypher-python-7a919a372be7 
 from pandas import DataFrame
 from neo4j import GraphDatabase
+import pandas as pd
+import matplotlib.pyplot as plt
+
 class Neo4jConnection:
     
     def __init__(self, uri, user, pwd):
@@ -39,10 +42,16 @@ conn.query("CREATE OR REPLACE DATABASE datathon")
 
 # Add Artists nodes
 query_string = '''
-LOAD CSV WITH HEADERS FROM "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/data/Artist.csv" AS csvLine
+LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/giuliabertoldo/datathon_2023_stat/main/artists.csv" AS csvLine
 CREATE (m:Artist {
     id: toInteger(csvLine.id), 
     name: csvLine.name,
+    last_name: csvLine.last_name,
+    birthplace: csvLine.birthplace,
+    deathplace: csvLine.deathplace,
+    birthdate: csvLine.birthdate,
+    deathdate: csvLine.deathdate,
+    cause_of_death: csvLine.cause_of_death,
     url: csvLine.url,
     summary: csvLine.summary
     })
@@ -51,7 +60,7 @@ conn.query(query_string, db='datathon')
 
 # Add edges 
 query_string = '''
-LOAD CSV WITH HEADERS FROM "https://kuleuven-datathon-2023.s3.eu-central-1.amazonaws.com/data/Apprenticeship.csv" AS csvLine
+LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/giuliabertoldo/datathon_2023_stat/main/appr.csv" AS csvLine
 MATCH (student:Artist {id: toInteger(csvLine.student_id)}), (teacher:Artist {id: toIntegerOrNull(csvLine.teacher_id)})
 MERGE (student) -[r:APPRENTICE_OF]-> (teacher)
 '''
@@ -92,7 +101,7 @@ conn.query(query_string, db='datathon')
 # Create dataframe with name and degree score
 query_string = '''
 MATCH (p:Artist)
-RETURN DISTINCT p.name, p.degree
+RETURN DISTINCT p.name, p.last_name, p.degree
 '''
 
 df_degree = DataFrame([dict(_) for _ in conn.query(query_string, db='datathon')])
@@ -105,13 +114,37 @@ top_twenty_degree = df_degree_sorted[0:20]
 
 top_fifty_degree = df_degree_sorted[0:50]
 
-# Export to csv 
+# Count degrees
+counts = df_degree.value_counts(subset=['p.degree'])
+df_counts = pd.DataFrame(counts)
+df_counts = df_counts.rename(columns={0: 'count'})
+df_counts['degree'] = [0, 1, 2]
 
-df_degree.to_csv('degree_centrality.csv')
+# Visualize
+fig, ax = plt.subplots()
+
+counts = [539, 35, 5]
+degree = ['0', '1', '2']
+
+ax.bar(degree, counts)
+
+ax.set_ylabel('Number of artists')
+ax.set_xlabel('Degree Centrality')
+
+plt.show()
 
 # BETWEENESS 
+# Create graph projection
 query_string = '''
-CALL gds.betweenness.write('graph1', { writeProperty: 'betweenness' })
+CALL gds.graph.project.cypher(
+  'graph2',
+  'MATCH (n:Artist) RETURN id(n) AS id',
+  'MATCH (n:Artist)-[r:APPRENTICE_OF]-(m:Artist) RETURN id(n) AS source, id(m) AS target')
+'''
+conn.query(query_string, db='datathon')
+
+query_string = '''
+CALL gds.betweenness.write('graph2', { writeProperty: 'betweenness' })
 YIELD centralityDistribution, nodePropertiesWritten
 RETURN centralityDistribution.min AS minimumScore, centralityDistribution.mean AS meanScore, nodePropertiesWritten
 
@@ -130,6 +163,7 @@ df_betweenness_sorted = df_betweenness.sort_values(by=['p.betweenness'], ascendi
 
 # Top 10 most higher df_betweenness_sorted artists
 top_ten_df_betweenness_sorted  = df_betweenness_sorted[0:10]
+
 
 
 
