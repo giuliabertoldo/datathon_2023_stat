031e79a693f69c7d4fda45c6ccb015acc45adc41
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-02-22 10:11:37 +0100

    Delete duplicate artists

--- clean_artists.py
+++ clean_artists.py
@@ -1,33 +1,39 @@
 import pandas as pd
 
-# Functions 
-def clean_artist_name(df): 
-    # name: lowercase
-    df['name'] = df['name'].apply(str.lower)
-
-    # Remove leading and trailing whitespaces
-    df['name'] = df['name'].str.strip()
-
-    # Remove (after)
-    df['name']= df['name'].str.replace('(after)', '')
-
-    # Remove round brackets
-    df['name']= df['name'].str.replace('(', '')
-    df['name']= df['name'].str.replace(')', '')
-
-    # Remove leading and trailing whitespaces
-    df['name'] = df['name'].str.strip()
-
-    # Save in csv file 
-    df.to_csv('cleaned_df.csv')
 
 # Read data
 artists = pd.read_csv("data/Artist.csv")
 
-# Clean artists
-clean_artist_name(artists)
+# name: lowercase
+artists['name'] = artists['name'].apply(str.lower)
+
+# Remove leading and trailing whitespaces
+artists['name'] = artists['name'].str.strip()
+
+# Remove (after)
+artists['name']= artists['name'].str.replace('(after)', '')
+
+# Remove round brackets
+artists['name']= artists['name'].str.replace('(', '')
+artists['name']= artists['name'].str.replace(')', '')
+
+# Remove leading and trailing whitespaces
+artists['name'] = artists['name'].str.strip()
+
+# create last_name variables 
+for i in range(1, 6, 1):
+    artists.loc[artists['name'].str.split().str.len() == i, 'last name'] = artists['name'].str.split().str[-1]
+# check that no null values are in last_name
+sum(artists['last name'].isnull())
+
+# which rows have same last_name?
+duplicated = artists[artists['last name'].duplicated()]
+
+# keep only one artist with specified last name
+artists = artists.drop_duplicates(subset='last name')
 
 
-
+ # Save in csv file 
+artists.to_csv('cleaned_df.csv')
 
 
